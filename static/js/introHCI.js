f7af887f703f3f578944a06da277000a9e39e884
Hillary <hthi@ucsd.edu>
2019-01-24 08:31:43 -0800

    Up to step 51

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,18 +1,29 @@
 'use strict';
 
-// Call this function when the page loads (the "ready" event)
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+ 	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description' id='toggle'><p>Description of the project.</p></div>");
+    } else {
+     	$("#toggle").toggle();
+    }
+}
+
+function initializePage(){
+	$("#testjs").click(function(e){
+		$('.jumbotron h1').text("Javascript is connected");
+		$('#testjs').text("Please wait...");
+		$(".jumbotron p").addClass("active");
+	});
+	$("a.thumbnail").click(projectClick);
+}
+
 $(document).ready(function() {
 	initializePage();
 })
-
-/*
- * Function that is called when the document is ready.
- */
-function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
-
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
-}
